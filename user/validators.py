be42caf9f8a9c1446551f397621ec56275e46aa2
johann Bacha <johannbacha@gmail.com>
2022-09-10 14:22:15 +0200

    user/validators.py

---
+++ user/validators.py
@@ -0,0 +1,9 @@
+from django.core.exceptions import ValidationError
+
+
+class ContainsLetterValidator:
+    def validate(self, password, user=None):
+        pass
+
+    def get_help_text(self):
+        pass
