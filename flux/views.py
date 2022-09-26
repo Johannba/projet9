722e57415f0cedd04a023bb5f43bc17feeb63a2b
johann <johannbacha@gmail.com>
2022-09-26 10:18:24 +0200

    erreur id

--- flux/views.py
+++ flux/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Value, CharField
 from django.shortcuts import redirect, render
+from itertools import chain
 from . import forms
 from . import models
 
@@ -32,12 +34,25 @@ def create_review(request):
 
 
 def posts(request):
-    return render(request, 'flux/posts.html')
+    tickets = models.Ticket.objects.filter(user=request.user)
+    reviews = models.Review.objects.filter(user=request.user)
+
+    tickets = tickets.annotate(contente_type=Value('TICKET', CharField()))
+    reviews = reviews.annotate(contente_type=Value('REVIEW', CharField()))
+
+    posts = sorted(chain(tickets, reviews), key=lambda post: post.time_created, reverse=True)
+    return render(request, 'flux/posts.html', context={'posts': posts})
 
 
 def flux(request):
-    tickets = models.Ticket.objects.all()
-    return render(request, 'flux/flux.html', context={'tickets': tickets})
+    tickets = models.Ticket.objects.filter(user=request.user)
+    reviews = models.Review.objects.filter(user=request.user)
+
+    tickets = tickets.annotate(contente_type=Value('TICKET', CharField()))
+    reviews = reviews.annotate(contente_type=Value('REVIEW', CharField()))
+
+    posts = sorted(chain(tickets, reviews), key=lambda post: post.time_created, reverse=True)
+    return render(request, 'flux/flux.html', context={'posts': posts})
 
 
 def review_flux(request):
@@ -56,3 +71,25 @@ def response_ticket(request, ticket_id):
             review.save()
             return redirect("review_form")
     return render(request, 'flux/create_review.html', context={'form': form})
+
+
+def ticket_review(request):
+    ticket_form = forms.TicketForm()
+    review_form = forms.ReviewForm()
+    if request.method == 'POST':
+        ticket_form = forms.TicketForm(request.POST, request.FILES)
+        review_form = forms.ReviewForm(request.POST, request.FILES)
+        if all([ticket_form.is_valid(), review_form.is_valid()]):
+            ticket = ticket_form.save(commit=False)
+            ticket.user = request.user
+            ticket = ticket.save()
+            review = review_form.save(commit=False)
+            review.user = request.user
+            review.ticket = ticket.id
+            review.save()
+            return redirect("flux")
+    context = {
+        'ticket_form': ticket_form,
+        'review_form': review_form,
+    }
+    return render(request, 'flux/ticket_review.html', context=context)
--- litreview/urls.py
+++ litreview/urls.py
@@ -22,7 +22,8 @@ urlpatterns = [
     path('create_ticket/', flux.views.create_ticket, name='create_ticket'),
     path('create_review/', flux.views.create_review, name='create_review'),
     path('review_form/', flux.views.review_flux, name='review_form'),
-    path('response_ticket/<int:ticket_id>/', flux.views.response_ticket, name='response_ticket')
+    path('response_ticket/<int:ticket_id>/', flux.views.response_ticket, name='response_ticket'),
+    path('ticket_review', flux.views.ticket_review, name='ticket_review'),
 ]
 
 if settings.DEBUG:
