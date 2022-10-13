8eb9a7be7fbea581a1fa8a8265f69f270e3eeceb
johann <johannbacha@gmail.com>
2022-10-13 13:57:28 +0200

    probleme is_reviewed

--- follower/views.py
+++ follower/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
-0
+
 def followers(request):
+    if request.POST:
+        username = request.POST['username']
+        return redirect("followers")
     return render(request, 'followers/followers.html')
